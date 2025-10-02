3cd97c43bb0b3189de8c38beba62770b91064099
isGoodSoup <sargent.pointed453@passinbox.com>
2025-10-02 13:23:53 +0200

    feat: icon with coords

--- src/com/kingdomtile/main/UI.java
+++ src/com/kingdomtile/main/UI.java
@@ -3,23 +3,46 @@ package com.kingdomtile.main;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class UI {
+	private BufferedImage image;
 	private Panel panel;
 	private Font font;
 	private int playerX;
 	private int playerY;
+	private int iconX, iconY;
+	private int textX, textY;
+	private int iconWidth = 48;
+	private int iconHeight = iconWidth;
 	
 	public UI(Panel panel) {
 		this.panel = panel;
+		getIcon();
+	}
+	
+	public void getIcon() {
+		try {
+			image = ImageIO.read(getClass().getResourceAsStream("/com/kingdomtile/ui/icon.png"));
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public void draw(Graphics2D g2) {
-		g2.setFont(setFont(new Font("Arial", Font.BOLD, 20)));
-		g2.setColor(Color.white);
+		iconX = 20; iconY = 15;
+		textX = iconX + image.getWidth() + 40;
+		textY = iconY + image.getHeight() / 2 + 20;
+		
+		g2.setFont(setFont(new Font("Arial", Font.BOLD, 25)));
+		g2.setColor(Color.black);
 		playerX = panel.getPlayer().getX() / panel.getTileSize();
 		playerY = panel.getPlayer().getY() / panel.getTileSize() + 1;
-		g2.drawString("x: " + playerX + " y: " + playerY, 20, 30);
+		g2.drawString("x: " + playerX + " y: " + playerY, textX, textY);
+		g2.drawImage(image, iconX, iconY, iconWidth, iconHeight, panel);
 	}
 
 	public Panel getPanel() {
@@ -37,5 +60,17 @@ public class UI {
 	public Font setFont(Font font) {
 		this.font = font;
 		return font;
+	}
+
+	public BufferedImage getImage() {
+		return image;
+	}
+
+	public int getIconWidth() {
+		return iconWidth;
+	}
+
+	public int getIconHeight() {
+		return iconHeight;
 	}
 }
